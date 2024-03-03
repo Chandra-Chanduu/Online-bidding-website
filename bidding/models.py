cfa6816b887303fff4705480cc3ee3dab252f81f
Chandra <chandra.pirsingula@gmail.com>
2024-03-03 23:10:15 +0530

    Basic add product form

--- bidding/models.py
+++ bidding/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.TextField()
+    minimum_price=models.IntegerField()
+    price_interval=models.IntegerField()
+    image=models.ImageField()
--- bidding/views.py
+++ bidding/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ProductForm
 
 # Create your views here.
 
 def home(request):
     return render(request,"home.html")
+
+def add_product(request):
+    form=ProductForm(request.POST or None)
+    if form.is_valid():
+        result=form.save()
+    return render(request,"auction/add_product.html",{'form':form})
+
